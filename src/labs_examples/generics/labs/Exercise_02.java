a0cc50bfa924af8c7acec7536b7c38ce4d8253a7
Ryan Desmond <radesmond@gmail.com>
2019-04-05 12:47:19 +0800

    generics lab updates

--- src/labs_examples/generics/labs/Exercise_02.java
+++ src/labs_examples/generics/labs/Exercise_02.java
@@ -3,5 +3,6 @@ package labs_examples.generics.labs;
 /**
  * Generics Exercise 2:
  *
- *      Create a class with a generic method that takes in an array and prints out the value at a certain index.
+ *      Create a class with a generic method that takes in an ArrayList of any Numeric type and returns the sum of all
+ *      Numbers in the ArrayList
  */
--- src/labs_examples/generics/labs/Exercise_03.java
+++ src/labs_examples/generics/labs/Exercise_03.java
@@ -3,7 +3,15 @@ package labs_examples.generics.labs;
 /**
  * Generics Exercise 3:
  *
- *      Create a class with a generic method that accepts two generic arguments. This generic method should only accept
+ *      1) Write a generic method that accepts two generic arguments. This generic method should only accept
  *      arguments which are sublasses of Number. The generic method must return the sum (as a double) of whatever two
  *      numbers were passed in regardless of their type.
+ *
+ *      2) Write a generic method to count the number of elements in a collection of Strings that are palindromes
+ *
+ *      3) Write a generic method to exchange the positions of two different elements in an array.
+ *
+ *      4) Write a generic method to find the largest element within the range (begin, end) of a list.
+ *
+ *      5) 
  */
