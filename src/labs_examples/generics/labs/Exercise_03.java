45e04b7d3040d9dff83b0363a9adf3ba60759515
Ryan Desmond <radesmond@gmail.com>
2020-02-29 08:44:28 +0800

    adding detail to generics lab #3

--- src/labs_examples/generics/labs/Exercise_03.java
+++ src/labs_examples/generics/labs/Exercise_03.java
@@ -10,15 +10,10 @@ import java.util.Collection;
  *      arguments which are sublasses of Number. The generic method must return the sum (as a double) of whatever two
  *      numbers were passed in regardless of their type.
  *
- *      2) Write a generic method to count the number of elements in a collection of Strings that are palindromes
- *
+ *      2) Write a generic method to count the number of elements in a "Collection" of Strings that are palindromes
+
  *      3) Write a generic method to exchange the positions of two different elements in an array.
  *
  *      4) Write a generic method to find the largest element within the range (begin, end) of a list.
  *
- *      5)
  */
-
-public class Exercise_03 {
-
-}
