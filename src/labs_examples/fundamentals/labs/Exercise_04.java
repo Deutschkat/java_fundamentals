6e52251c1f7376f9a4fa54266cf41f9729481e7b
Ryan Desmond <radesmond@gmail.com>
2020-05-05 15:12:41 -0700

    updating fundamentals labs

---
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -0,0 +1,22 @@
+package labs_examples.fundamentals.labs;
+
+/**
+ * Fundamentals Exercise 4: My Second Program
+ *
+ *      Write the necessary code to display declare and print each of Java's primitive data types.
+ *      Please declare a single variable of each type, give it an appropriate valyue, then print that variable.
+ *
+ */
+
+public class Exercise_04 {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+        System.out.println("int i is: " + i);
+
+        // write your code below
+
+    }
+
+}
---
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -0,0 +1,29 @@
+package labs_examples.fundamentals.labs;
+
+
+/**
+ * Fundamentals Exercise 5: Working with Strings
+ *
+ *      Please follow the instructions in the comments below
+ *
+ */
+
+public class Exercise_05 {
+
+    public static void main(String[] args) {
+
+        String str = "hello!";
+        // please declare an int variable below, and set it to the value of the length of "str"
+
+        String str2 = "hello";
+        // please initialize a boolean variable and test whether str is equal to str2
+
+        // please concatenate str & str2 and set the result to a new String variable below
+
+        // please demonstrate the use of any other method that is available to us in the String class
+        // for example, replace(), substring(), contains(), indexOf() etc
+
+    }
+
+
+}
