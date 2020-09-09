e6093c9093e159ebaeb26d25183a47e84e09b7ed
CodingNomads Dev <ryan@codingnomads.co>
2020-09-09 10:15:38 -0700

    Update Exercise_03.java

--- src/labs_examples/arrays/labs/Exercise_03.java
+++ src/labs_examples/arrays/labs/Exercise_03.java
@@ -18,15 +18,6 @@ package labs_examples.arrays.labs;
 public class Exercise_03 {
 
     public static void main(String[] args) {
-        int[][] twoDArray = new int[5][5];
-        // use 2 for loops to populate the 2d array
-        // first for loop
-        for (int innerLoop = 1; innerLoop < twoDArray.length; innerLoop++){
-            for (int outerLoop = 0; outerLoop < twoDArray[innerLoop].length; outerLoop++){
-                twoDArray[innerLoop][outerLoop] = innerLoop * 3;
-                System.out.print(twoDArray[outerLoop][innerLoop]);
-            }
-            System.out.println();
-        }
+        
     }
 }
