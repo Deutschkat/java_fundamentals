fc36c4b1b8c5d2bc07ff287dc39939c4a3926817
CodingNomads Dev <ryan@codingnomads.co>
2020-09-15 15:37:11 -0700

    Update Exercise_02.java

--- src/labs_examples/conditions_loops/labs/Exercise_02.java
+++ src/labs_examples/conditions_loops/labs/Exercise_02.java
@@ -4,7 +4,10 @@ package labs_examples.conditions_loops.labs;
  * Conditions and Loops Exercise 2: Days of the week
  *
  *      Take in a number from the user and print "Monday", "Tuesday", ... "Sunday", or "Other"
- *      if the number from the user is 1, 2,... 7, or other respectively. Use a "nested-if" statement.
+ *      if the number from the user is 1, 2,... 7, or other respectively. Use an if-else statement
+ *      to accomplish this task.
+ * 
+ *      Bonus Tricky Challenge: Use a "nested-if" statement.
  *
  */
 
