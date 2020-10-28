d338e8aed488b3d29b0ad19083dd57bb713c6697
Ryan Desmond <ryan@codingnomads.co>
2020-10-28 16:26:12 -0700

    adding files examples for Stream Api

--- src/labs_examples/lambdas/examples/stream_api/JavaStreams.java
+++ src/labs_examples/lambdas/examples/stream_api/JavaStreams.java
@@ -12,13 +12,15 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /*
+    Java Stream API
+
     source: https://raw.githubusercontent.com/joeyajames/Java/master/Java%208%20Streams/JavaStreams.java
 
     Stream steps:
     1) Obtain a stream
     2) call zero or more "non-terminal" operations on the stream
         * non-terminal operations include things like:
-            - map
+            - map (aka modify)
             - filter
             - sorted
             - etc
@@ -36,45 +38,50 @@ public class JavaStreams {
     public static void main(String[] args) throws IOException {
 
         // 1. Integer Stream
+        System.out.println("\n---------- Example 1 ------------");
         IntStream
                 .range(1, 10)
                 .forEach(System.out::print);
-        System.out.println();
 
         // 2. Integer Stream with skip
+        System.out.println("\n---------- Example 2 ------------");
         IntStream
                 .range(1, 10)
                 .skip(5)
                 .forEach(x -> System.out.println(x));
-        System.out.println();
 
         // 3. Integer Stream with sum
+        System.out.println("\n---------- Example 3 ------------");
         System.out.println(IntStream.range(1, 5).sum());
-        System.out.println();
 
         // 4. Stream.of, sorted and findFirst
+        System.out.println("\n---------- Example 4 ------------");
         Stream.of("Ava", "Aneri", "Alberto")
                 .sorted()
                 .findFirst()
                 .ifPresent(System.out::println);
 
         // 5. Stream from Array, sort, filter and print
+        System.out.println("\n---------- Example 5 ------------");
         String[] names = {"Al", "Ankit", "Kushal", "Brent", "Sarika", "Amanda", "Hans", "Shivika", "Sarah"};
         Arrays.stream(names)	// same as Stream.of(names)
                 .filter(x -> x.startsWith("S"))
                 .sorted()
                 .forEach(System.out::println);
 
-        Stream<String> stream = Arrays.stream(names);
-        stream.forEach((String element) -> System.out.print(element));
+        //Extra demo
+        //Stream<String> stream = Arrays.stream(names);
+        //stream.forEach((String element) -> System.out.print(element));
 
         // 6. average of squares of an int array
+        System.out.println("\n---------- Example 6 ------------");
         Arrays.stream(new int[] {2, 4, 6, 8, 10})
                 .map(x -> x * x)
                 .average()
                 .ifPresent(System.out::println);
 
         // 7. Stream from List, filter and print
+        System.out.println("\n---------- Example 7 ------------");
         List<String> people = Arrays.asList("Al", "Ankit", "Brent", "Sarika", "Amanda", "Hans", "Shivika", "Sarah");
         people
                 .stream()
@@ -82,9 +89,13 @@ public class JavaStreams {
                 .filter(x -> x.startsWith("a"))
                 .forEach(System.out::println);
 
-        String bandsFile = "src/resources/bands.txt";
-        String dataFile = "src/resources/data.txt";
+
+        // declare paths to data files in this examples_files directory
+        String bandsFile = "src/labs_examples/lambdas/examples/stream_api/example_files/bands.txt";
+        String dataFile = "src/labs_examples/lambdas/examples/stream_api/example_files/data.txt";
+
         // 8. Stream rows from text file, sort, filter, and print
+        System.out.println("\n---------- Example 8 ------------");
         Stream<String> bands = Files.lines(Paths.get(bandsFile));
         bands
                 .sorted()
@@ -93,6 +104,7 @@ public class JavaStreams {
         bands.close();
 
         // 9. Stream rows from text file and save to List
+        System.out.println("\n---------- Example 10 ------------");
         List<String> bands2 = Files.lines(Paths.get(bandsFile))
                 .filter(x -> x.contains("jit"))
                 .collect(Collectors.toList());
@@ -102,6 +114,7 @@ public class JavaStreams {
 
 
         // 10. Stream rows from CSV file and count
+        System.out.println("\n---------- Example 10 ------------");
         Stream<String> rows1 = Files.lines(Paths.get(dataFile));
         int rowCount = (int)rows1
                 .map(x -> x.split(","))
@@ -111,6 +124,7 @@ public class JavaStreams {
         rows1.close();
 
         // 11. Stream rows from CSV file, parse data from rows
+        System.out.println("\n---------- Example 11 ------------");
         Stream<String> rows2 = Files.lines(Paths.get(dataFile));
         rows2
                 .map(x -> x.split(","))
@@ -120,6 +134,7 @@ public class JavaStreams {
         rows2.close();
 
         // 12. Stream rows from CSV file, store fields in HashMap
+        System.out.println("\n---------- Example 12 ------------");
         Stream<String> rows3 = Files.lines(Paths.get(dataFile));
         Map<String, Integer> map = new HashMap<>();
         map = rows3
@@ -135,8 +150,10 @@ public class JavaStreams {
         }
 
         // 13. Reduction - sum
+        System.out.println("\n---------- Example 13 ------------");
         double total = Stream.of(7.3, 1.5, 4.8)
                 .reduce(0.0, (Double a, Double b) -> a + b);
+
         System.out.println("Total = " + total);
     }
 }
