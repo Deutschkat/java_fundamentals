7a7b267d489e0f4b77aa83c296e24261923cacf4
CodingNomads Dev <ryan@codingnomads.co>
2020-11-16 08:48:30 -0800

    Update Exercise_02.java

--- src/labs_examples/datastructures/stack/labs/Exercise_02.java
+++ src/labs_examples/datastructures/stack/labs/Exercise_02.java
@@ -15,4 +15,6 @@ package labs_examples.datastructures.stack.labs;
  *      5) contain a size() method
  *      6) contain a method to print out the data of all elements in the Stack
  *
+ *      TIP: To initialize an new array of a Generic type you can use this:
+ *           Object[] data = (T[]) new Object[10];
  */
