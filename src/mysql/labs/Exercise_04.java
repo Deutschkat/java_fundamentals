665382b4d91f9a68e589781df2c6128b2ef0ba1e
CodingNomads Dev <ryan@codingnomads.co>
2021-12-10 10:01:43 -0800

    Update Exercise_04.java

--- src/mysql/labs/Exercise_04.java
+++ src/mysql/labs/Exercise_04.java
@@ -15,7 +15,7 @@ package mysql.labs;
 *       3.) Bonus: flex those JDBC muscles! How nice, clean, useful can you make this? Show us what you've got!
 *
 *
-*   Caution: Before you push this code to GitHub, be sure to REMOVE or REPLACE your DATBASE PASSWORD. You do not 
+*   Caution: Before you push this code to GitHub, be sure to REMOVE or REPLACE your DATABASE PASSWORD. You do not 
 *   want to push your database password to GitHub. If you do push your password to GitHub you'll need to delete
 *   the commit from your Git history. See, for instance: https://medium.com/multinetinventiv/removing-commits-from-git-history-4e2340288484 
 *
