fd20018bfd97c02652d151cf5c4566ab0ca51871
Kat Nix <deutschkat29@gmail.com>
2022-10-22 17:35:52 -0500

    Completed exercise 04

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -12,10 +12,25 @@ public class Exercise_04 {
 
     public static void main(String[] args) {
 
-        int i = 1;
-        System.out.println("int i is: " + i);
+        System.out.println("Kat"+ "'"+"s" + " code below:");
 
-        // write your code below
+        byte a = 12;
+        short b = 30000;
+        int c = 500000;
+        long d = 812234534332L;
+        float e = 3.2456f;
+        double f = 4.567567567;
+        char letter = 'K';
+        boolean thisWorks = true;
+
+        System.out.println("Byte a equals: " + a);
+        System.out.println("Short b equals: " + b);
+        System.out.println("Int c equals: " + c);
+        System.out.println("Long d equals: " + d);
+        System.out.println("Float e equals: " + e);
+        System.out.println("Double f equals: " + f);
+        System.out.println("Char equals: " + letter);
+        System.out.println("Boolean thisWorks equals: " + thisWorks);
 
     }
 
