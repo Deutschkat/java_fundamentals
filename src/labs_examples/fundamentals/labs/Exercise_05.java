b86251c6866f4f97f4fdd850045fe715fc09224b
Kat Nix <deutschkat29@gmail.com>
2022-10-22 18:14:32 -0500

    Completed exercise 05

--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,14 +14,19 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int length = "hello!".length();
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
+        boolean equals = "hello!".equals(str2);
 
         // please concatenate str & str2 and set the result to a new String variable below
+        System.out.println(str + " " + str2);
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+        int search = "hello!".indexOf("o");
+
 
     }
 
