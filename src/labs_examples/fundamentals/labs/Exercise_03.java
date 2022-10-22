39b6cf561b1ef185de25ba2f27b4d78803d94497
Kat Nix <deutschkat29@gmail.com>
2022-10-22 16:37:54 -0500

    Completed exercise 03

--- src/labs_examples/fundamentals/labs/Exercise_03.java
+++ src/labs_examples/fundamentals/labs/Exercise_03.java
@@ -15,8 +15,7 @@ public class Exercise_03 {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
-
-        // write code to print out "Check out my first program!" below
+        System.out.println("Check out my first program!");
 
     }
 
