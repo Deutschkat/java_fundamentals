95c8b6ff002be2d34bcc150116348790e9d7bb9c
Kat Nix <deutschkat29@gmail.com>
2022-10-25 21:23:50 -0500

    exercise 06

--- src/labs_examples/datatypes_operators/labs/Exercise_06.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_06.java
@@ -12,7 +12,24 @@ public class Exercise_06 {
 
     public static void main(String[] args) {
 
-        // write code here
+        float pi = 3.14f;
+        float rad = 3.14f;
+        int height = 5;
+
+        float vol = (pi * ((rad * rad) * height));
+        System.out.println(vol);
+
+        float surfaceArea1 = ((2) * (pi) * (rad) * (5));
+        float surfaceArea2 = ((2) * (pi) * (rad) * (rad));
+        float totalSurfaceArea = surfaceArea1 + surfaceArea2;
+        System.out.println(totalSurfaceArea);
+
+        // Sorry, I'm not great with math, and I know there is probably a cleaner way to type these
+        // out but I knew this would get the job done.
+
+
+
+
 
     }
 }
