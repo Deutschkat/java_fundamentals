ee14fd00428b56b489c42f106ee88ae658c77f0d
Kat Nix <deutschkat29@gmail.com>
2022-10-30 16:37:04 -0500

    Repl If Statements

---
+++ src/labs_examples/repl/MainIfStatements.java
@@ -0,0 +1,22 @@
+package labs_examples.repl;
+
+public class MainIfStatements {
+
+    public static void main(String[] args) {
+        // please write and run atleast 3 unique if statements below
+
+        int i = 10;
+
+        if (i < 20){
+            System.out.println("i is less than 20!");
+        }
+
+        if (i > 5){
+            System.out.println("i is greater than 5!");
+        }
+
+        if (i == 10){
+            System.out.println("i equals 10!");
+        }
+    }
+}
