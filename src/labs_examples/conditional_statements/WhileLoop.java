aad49fc58d35cca14f17abbe0f2f41cb01fb72a1
Kat Nix <deutschkat29@gmail.com>
2022-10-30 17:08:51 -0500

    while loop

---
+++ src/labs_examples/conditional_statements/WhileLoop.java
@@ -0,0 +1,16 @@
+package labs_examples.conditional_statements;
+
+public class WhileLoop {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+
+        while(count < 10){
+            System.out.println("count is " + count);
+            count++;
+        }
+
+        System.out.println("all done with while loop!");
+    }
+        }
