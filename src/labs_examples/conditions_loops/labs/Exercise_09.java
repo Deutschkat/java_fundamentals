169b5afae6543af2d553a7e3cc0a90b3ada84d7d
Kat Nix <deutschkat29@gmail.com>
2022-11-04 01:27:23 -0500

    exercise 09 finished

--- src/labs_examples/conditions_loops/labs/Exercise_09.java
+++ src/labs_examples/conditions_loops/labs/Exercise_09.java
@@ -8,4 +8,17 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_09 {
+    public static void main(String[] args) {
+
+        int y = 20;
+
+        while(y <= 35){
+            y++;
+            if(y == 27){
+                break;
+            }
+            System.out.println(y);
+        }
+
+    }
 }
