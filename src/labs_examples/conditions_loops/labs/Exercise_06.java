5e59019b7ffe7939291ef607b2f75dc0d5ae3df7
Kat Nix <deutschkat29@gmail.com>
2022-11-04 00:15:47 -0500

    exercise 06

--- src/labs_examples/conditions_loops/labs/Exercise_06.java
+++ src/labs_examples/conditions_loops/labs/Exercise_06.java
@@ -8,4 +8,16 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_06 {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+        int sum = 0;
+
+        while(i < 100){
+            i++;
+            sum += i;
+        }
+        System.out.println("The sum is: " + sum);
+    }
 }
