0b47c373281f9bf932a1573f12d579c26db9007d
Kat Nix <deutschkat29@gmail.com>
2022-11-03 22:08:41 -0500

    exercise 03

--- src/labs_examples/conditions_loops/labs/Exercise_03.java
+++ src/labs_examples/conditions_loops/labs/Exercise_03.java
@@ -1,5 +1,7 @@
 package labs_examples.conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 3: Months of the year
  *
@@ -9,5 +11,55 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_03 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter a number that coordinates with that specific month of the year: ");
+
+        int month = scanner.nextInt();
+
+        switch(month){
+            case 1:
+                System.out.println("January");
+                break;
+            case 2:
+                System.out.println("February");
+                break;
+            case 3:
+                System.out.println("March");
+                break;
+            case 4:
+                System.out.println("April");
+                break;
+            case 5:
+                System.out.println("May");
+                break;
+            case 6:
+                System.out.println("June");
+                break;
+            case 7:
+                System.out.println("July");
+                break;
+            case 8:
+                System.out.println("August");
+                break;
+            case 9:
+                System.out.println("September");
+                break;
+            case 10:
+                System.out.println("October");
+                break;
+            case 11:
+                System.out.println("November");
+                break;
+            case 12:
+                System.out.println("December");
+                break;
+            default:
+                System.out.println("There are only 12 months in the year. Please enter a number from 1 to 12.");
+                break;
+        }
+    }
 
 }
