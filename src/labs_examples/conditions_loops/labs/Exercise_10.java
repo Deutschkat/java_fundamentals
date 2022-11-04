52dd403e227fdb7195ee70c3ef38d141f30a6441
Kat Nix <deutschkat29@gmail.com>
2022-11-04 01:29:15 -0500

    exercise 10 finished

--- src/labs_examples/conditions_loops/labs/Exercise_10.java
+++ src/labs_examples/conditions_loops/labs/Exercise_10.java
@@ -8,4 +8,13 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_10 {
+    public static void main(String[] args) {
+
+        for(int j = 10; j < 20; j++){
+            if (j == 14){
+                continue;
+            }
+            System.out.println("j = " +j);
+        }
+    }
 }
