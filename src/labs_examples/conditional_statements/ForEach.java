9aaff3962487f0b475ac0e6381ac7db3af128207
Kat Nix <deutschkat29@gmail.com>
2022-11-03 19:11:16 -0500

    for each

---
+++ src/labs_examples/conditional_statements/ForEach.java
@@ -0,0 +1,10 @@
+package labs_examples.conditional_statements;
+
+public class ForEach {
+    public static void main(String[] args) {
+
+        for(String val : args){
+            System.out.println(val);
+        }
+    }
+}
