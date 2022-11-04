22013efead68e7d76333e77958e03dbdd39e11d6
Kat Nix <deutschkat29@gmail.com>
2022-11-04 01:19:39 -0500

    exercise 07 NOT DONE

--- src/labs_examples/conditions_loops/labs/Exercise_07.java
+++ src/labs_examples/conditions_loops/labs/Exercise_07.java
@@ -1,5 +1,7 @@
 package labs_examples.conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 7: First vowel
  *
@@ -14,4 +16,17 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_07 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter a word: ");
+
+        String userWord = scanner.next();
+
+        //DONT UNDERSTAND //
+
+    }
 }
+
+
