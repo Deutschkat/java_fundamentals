14ec5a3966de1959075c3fa5d260b1fb22947636
Kat Nix <deutschkat29@gmail.com>
2022-11-04 01:22:20 -0500

    exercise 08 finished

--- src/labs_examples/conditions_loops/labs/Exercise_08.java
+++ src/labs_examples/conditions_loops/labs/Exercise_08.java
@@ -8,4 +8,14 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_08 {
+    public static void main(String[] args) {
+
+        int x =1000;
+
+        do {
+            System.out.println(x);
+            x -= 3;
+        }while(x > 1);
+
+    }
 }
