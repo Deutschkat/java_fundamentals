7193170f45d200ddbdf25cdfef6a90ddb5a788e6
Kat Nix <deutschkat29@gmail.com>
2022-11-06 21:14:06 -0600

    example 01 exercise no double but it works

--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -1,6 +1,7 @@
 package labs_examples.arrays.labs;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * Arrays calculator
@@ -13,5 +14,45 @@ import java.util.Arrays;
  */
 
 public class Exercise_01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Please enter 10 numbers.");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] calc = new int [10];
+
+        calc[0] = scanner.nextInt();
+        calc[1] = scanner.nextInt();
+        calc[2] = scanner.nextInt();
+        calc[3] = scanner.nextInt();
+        calc[4] = scanner.nextInt();
+        calc[5] = scanner.nextInt();
+        calc[6] = scanner.nextInt();
+        calc[7] = scanner.nextInt();
+        calc[8] = scanner.nextInt();
+        calc[9] = scanner.nextInt();
+
+        // sum
+
+        int sum = 0;
+        for(int indexValue : calc){
+            sum += indexValue;
+        }
+
+        System.out.println("The sum of the numbers you have entered is: " + sum);
+
+        System.out.println();
+        System.out.println();
+
+        //average
+
+        int average = sum / 10;
+
+        System.out.println("The average of the numbers you have entered is: " + average);
+
+
+    }
 
 }
