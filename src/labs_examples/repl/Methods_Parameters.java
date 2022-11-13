be8bd14540f055745e0eee6ba43bb7e3360106c7
Kat Nix <deutschkat29@gmail.com>
2022-11-13 16:46:55 -0600

    methods

---
+++ src/labs_examples/repl/Methods_Parameters.java
@@ -0,0 +1,17 @@
+package labs_examples.repl;
+
+public class Methods_Parameters {
+    public static void main(String[] args) {
+
+
+    }
+
+    public static int multiply(int a, int b){
+        return a * b;
+    }
+
+    public static int divide(int a, int b){
+        return a / b;
+    }
+// HELP WITH REST OF THIS
+}
