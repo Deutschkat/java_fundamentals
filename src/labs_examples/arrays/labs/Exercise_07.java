4a60778287193cba3eb3babee4b4ae98ee13467a
Kat Nix <deutschkat29@gmail.com>
2022-11-12 23:05:21 -0600

    exercise 07

--- src/labs_examples/arrays/labs/Exercise_07.java
+++ src/labs_examples/arrays/labs/Exercise_07.java
@@ -1,5 +1,7 @@
 package labs_examples.arrays.labs;
 
+import java.util.ArrayList;
+
 /**
  *  ArrayLists
  *
@@ -10,4 +12,42 @@ package labs_examples.arrays.labs;
  *
  */
 public class Exercise_07 {
+    public static void main(String[] args) {
+
+        ArrayList<String> dogNames = new ArrayList<>();
+
+        dogNames.add("Indiana");
+        dogNames.add("Rasmus");
+        dogNames.add("Sammy Davis Jr. Jr.");
+        dogNames.add("Bandi");
+        dogNames.add("Kahlua");
+        dogNames.add("Lucky");
+        dogNames.add("Pumpkin");
+        dogNames.add("Sugar");
+        dogNames.add("Shady");
+        dogNames.add("Mia");
+        dogNames.add("Einstein");
+        dogNames.add("Tobi");
+
+        dogNames.remove("Pumpkin"); //Removed cat name from list of dog names.
+
+        for(String s : dogNames){ //Printed the list of dog names.
+            System.out.println(s);
+        }
+
+        System.out.println();
+
+        System.out.println("Is Einstein one of my dog's names?");
+
+        boolean check = dogNames.contains("Einstein");  //Check the list for a specific item.
+        System.out.println("Yes! Einstein is one of the names.");
+
+        System.out.println();
+        System.out.println("What index do we find Sugar at on the list? ");
+
+        int lastIndexOfSugar = dogNames.lastIndexOf("Sugar");
+        System.out.println("Sugar is found at the index of: " + lastIndexOfSugar);
+
+
+    }
 }
