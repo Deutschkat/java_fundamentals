64ac808d6c1376c3777e8b5f536ca6b644ee8aad
Kat Nix <deutschkat29@gmail.com>
2022-11-12 22:38:39 -0600

    exercise 05

--- src/labs_examples/arrays/labs/Exercise_05.java
+++ src/labs_examples/arrays/labs/Exercise_05.java
@@ -10,4 +10,29 @@ package labs_examples.arrays.labs;
 
 public class Exercise_05 {
 
+    public static void main(String[] args) {
+
+        int [][] reverse = new int[7][7];
+
+        int count = 48;
+
+        for(int i = 0; i < reverse.length; i++){   // switched the greater than to a less than and initialized at 48
+            for(int x = 0; x < reverse[i].length; x++){  // did same here to reverse order
+
+                reverse[i][x] = count;
+                count = count - 1;
+
+            }
+        }
+
+        for(int i = 0; i < reverse.length; i++){
+            for(int x = 0; x < reverse[i].length; x++){
+
+                System.out.print(reverse[i][x] + " | ");
+            }
+            System.out.println();
+        }
+
+    }
+
 }
