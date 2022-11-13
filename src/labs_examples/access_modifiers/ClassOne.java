6099dc1d769792de19e99caab03e65ca0fd4a112
Kat Nix <deutschkat29@gmail.com>
2022-11-13 15:48:52 -0600

    class one

--- src/labs_examples/access_modifiers/ClassOne.java
+++ src/labs_examples/access_modifiers/ClassOne.java
@@ -7,12 +7,21 @@ public class ClassOne {
         tenants of Object-Oriented Programming
 
         public
+            any class, instance variable or method that is marked as "public" can
+            be openly accessible from anywhere in the application. it can also be accessed by external
+            applications that are using this application.
 
         private
+            only accessible from within the class where the private variable or method is declared.
+            we do not have a thing such as a private class.
 
         protected
+            only accessible by classes (and their members) that are declared in the same package
+            as the variable or method that is declared "protected". in addition, subclasses whether
+            they are in the same package or not, can access protected members.
 
         default
+            same as protected but not accessible by subclasses.
 
 
      */
