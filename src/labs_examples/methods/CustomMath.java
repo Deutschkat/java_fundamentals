0fb4bc57d4103aa82ad10cc19756adee94bceb73
Kat Nix <deutschkat29@gmail.com>
2022-11-13 16:43:46 -0600

    methods

---
+++ src/labs_examples/methods/CustomMath.java
@@ -0,0 +1,12 @@
+package labs_examples.methods;
+
+public class CustomMath {
+
+    public static int multiply(int a, int b){
+        return a * b;
+    }
+
+    public static int divide(int a, int b){
+        return a / b;
+    }
+}
---
+++ src/labs_examples/methods/MathController.java
@@ -0,0 +1,16 @@
+package labs_examples.methods;
+
+public class MathController {
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 3;
+        int x = CustomMath.multiply(a, b);
+
+        System.out.println(x);
+
+    }
+}
+
+// VIDEO FOR METHOD PARAMETERS SUPER HELPFUL IF STUCK.
+//Ctrl click will take you to the multiply method.
