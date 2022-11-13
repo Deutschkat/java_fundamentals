964ab1757b993f6a6550228123ee5d90a86d3a9f
Kat Nix <deutschkat29@gmail.com>
2022-11-13 16:29:44 -0600

    methods

---
+++ src/labs_examples/repl/Methods.java
@@ -0,0 +1,13 @@
+package labs_examples.repl;
+
+public class Methods {
+    public static void main(String[] args) {
+
+        printMethod();
+
+    }
+
+    public static void printMethod(){
+        System.out.println("This is a print method that is being called to the main method!");
+    }
+}
