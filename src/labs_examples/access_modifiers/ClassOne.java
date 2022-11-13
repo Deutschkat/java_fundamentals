6e7371a67c7d87502b9aa0d235127dc85753d245
Kat Nix <deutschkat29@gmail.com>
2022-11-13 15:42:34 -0600

    class one

---
+++ src/labs_examples/access_modifiers/ClassOne.java
@@ -0,0 +1,19 @@
+package labs_examples.access_modifiers;
+
+public class ClassOne {
+
+    /* ACCESS MODIFIERS -affect classes, instance variables and methods
+        they play a large role in Encapsulation - one of the primary
+        tenants of Object-Oriented Programming
+
+        public
+
+        private
+
+        protected
+
+        default
+
+
+     */
+}
