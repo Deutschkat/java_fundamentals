3981769ff2cee8b99f6b213ecbeb96e4b78c02b1
Kat Nix <deutschkat29@gmail.com>
2022-11-12 22:50:35 -0600

    exercise 06

--- src/labs_examples/arrays/labs/Exercise_06.java
+++ src/labs_examples/arrays/labs/Exercise_06.java
@@ -17,10 +17,12 @@ public class Exercise_06 {
     public static void main(String[] args){
         int[] nums = {45, 87, 62, 99, 1, 42, 12};
 
-        int temp;
-
         // Question: in the for loop below, why do we divide "nums.length" by 2?
         for(int i = 0; i < nums.length / 2; i++){
+            int temp = nums[i];
+            nums[i] = nums[nums.length - 1 - i];
+            nums[nums.length - 1 - i] = temp;
+
             // swap elements at indexes in array
             // you have two indices readily available for you to use "i" (which increments) and "nums.length"
             // you've also got this "temp" variable you can use to hold a value temporarily
