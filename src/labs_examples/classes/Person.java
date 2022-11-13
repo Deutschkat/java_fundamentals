e2757f0b4c7461a7d53582aa8a4d67488810806f
Kat Nix <deutschkat29@gmail.com>
2022-11-12 23:16:44 -0600

    person

---
+++ src/labs_examples/classes/Person.java
@@ -0,0 +1,17 @@
+package labs_examples.classes;
+
+public class Person {
+
+    // instance variable
+    int age;
+    double height;
+    String name;
+
+
+    // constructors
+    //...
+
+    // methods
+    //...
+}
+
