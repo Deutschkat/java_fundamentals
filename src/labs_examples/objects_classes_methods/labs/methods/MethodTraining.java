7b75977dc4272ed92472b6673e48cdf3da4882ea
Kat Nix <deutschkat29@gmail.com>
2022-11-20 18:15:14 -0600

    excercise 02 not finished

--- src/labs_examples/objects_classes_methods/labs/methods/MethodTraining.java
+++ src/labs_examples/objects_classes_methods/labs/methods/MethodTraining.java
@@ -2,20 +2,25 @@ package labs_examples.objects_classes_methods.labs.methods;
 
 import labs_examples.classes.Person;
 
+import java.util.Scanner;
+
 public class MethodTraining {
     public static void main(String[] args) {
 
+        //Pass By Ref (2)
         System.out.println("Pass by Reference Test: ");
         Person kat = new Person(30, 66, "Kat");
         System.out.println(kat.toString());
-        add2YearsToPerson(kat);             // Pass By Ref
+        add2YearsToPerson(kat);
         System.out.println(kat.toString());
 
-        System.out.println(); //Space for clarity.
+        //Space for Clarity
+        System.out.println();
         System.out.println();
 
+        //Pass By Val (2)
         float a = 0.12345f;
-        float b = 6.78910f; //Pass by Val
+        float b = 6.78910f;
 
         System.out.println("Pass by Val Test: ");
         System.out.println("A and B after PassByVal Test");
@@ -23,15 +28,50 @@ public class MethodTraining {
         System.out.println("A without method: " + a);
         System.out.println("B without method: " + b);
 
+        //Space for Clarity
         System.out.println();
         System.out.println();
 
-        highestNum(10, 4, 6, 3);
-        System.out.println(highestNum(10, 4, 6, 3));
 
+        // Largest Num (3)
+        System.out.println("Please enter 4 numbers to find largest: ");
+        Scanner scanner = new Scanner(System.in);
+
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+        int num4 = scanner.nextInt();
+
+        highestNum(num1, num2, num3, num4);
+
+        //Space for Clarity
+        System.out.println();
+        System.out.println();
+
+        // Method to Count All Consonants (4)
+
+        consonantCount();
+
+        System.out.println();
+        System.out.println();
+
+        //Prime or Not? (5)
+
+        System.out.println("Please enter a number to find out if it is prime or not: ");
+        Scanner primeScanner = new Scanner(System.in);
+        int num = primeScanner.nextInt();
+        if (primeOrNot(num))
+            System.out.println(num + " is a prime number!");
+        else
+            System.out.println(num + " is not a prime number!");
+
+
+        //Space
+        System.out.println();
+        System.out.println();
     }
 
-    //Method Overloading
+    //Method Overloading (1)
     public static int multiply(int a, int b){
         return a * b;
     }
@@ -56,10 +96,67 @@ public class MethodTraining {
         System.out.println(b);
     }
 
-    public static int highestNum(int... args){
 
+    //Was the way I did highestNum okay? I made it void as you can see.
+    public static void highestNum(int num1, int num2, int num3, int num4){
+        if(num1 >= num2)
+            if(num1 >= num3)
+                if(num1 >= num4)
+                    System.out.println(num1 + " is the largest number entered.");
+        if(num2 >= num1)
+            if(num2 >= num3)
+                if(num2 >= num4)
+                    System.out.println(num2 + " is the largest number entered.");
+        if(num3 >= num1)
+            if(num3 >= num2)
+                if(num3 >= num4)
+                    System.out.println(num3 + " is the largest number entered.");
+        if(num4 >= num1)
+            if(num4 >= num2)
+                if(num4 >= num3)
+                    System.out.println(num4 + " is the largest number entered.");
 
         }
-    }
+
+        public static void consonantCount(){
+
+        int count = 0;
+            System.out.println("Please enter a word or sentence to count its consonants: ");
+            Scanner consonantScanner = new Scanner(System.in);
+            String userInput = consonantScanner.nextLine();
+
+            for(int i = 0; i < userInput.length(); i++) {
+                char ch = userInput.charAt(i);
+                if (ch == 'a') {
+                    System.out.println("");
+                } else if (ch == 'e') {
+                    System.out.println("");
+                } else if (ch == 'i') {
+                    System.out.println("");
+                } else if (ch == 'o') {
+                    System.out.println("");
+                } else if (ch == 'u') {
+                    System.out.println("");
+                } else if(ch != ' ') {
+                    count++;
+                }
+            }
+            System.out.println("The number of consonants is: " + count);
+
+        }
+
+        public static boolean primeOrNot(int num){
+
+        if(num <= 1)
+            return false;
+
+        for(int i = 2; i < num; i++)
+            if(num % i ==0)
+                return false;
+
+        return true;
+
+        }
 
 }
+
