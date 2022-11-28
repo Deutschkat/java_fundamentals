af3c63ef349e1b4ff27b38110c55e9a52fbc9a78
Kat Nix <deutschkat29@gmail.com>
2022-11-28 16:21:05 -0600

    encapsulation stuff

---
+++ src/labs_examples/oop/encapsulation/animals/Dog.java
@@ -0,0 +1,34 @@
+package labs_examples.oop.encapsulation.animals;
+
+// class level is the root of all encapsulation
+public class Dog {
+
+// access modifiers are the next level of encapsulation
+    private String breed;
+    private String name;
+    private int age;
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/labs_examples/oop/encapsulation/city/City.java
@@ -0,0 +1,32 @@
+package labs_examples.oop.encapsulation.city;
+
+public class City {
+
+    private String name;
+    private int population;
+    private String latLong;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public String getLatLong() {
+        return latLong;
+    }
+
+    public void setLatLong(String latLong) {
+        this.latLong = latLong;
+    }
+}
