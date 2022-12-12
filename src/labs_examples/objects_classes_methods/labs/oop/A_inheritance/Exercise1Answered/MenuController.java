0d35d88fe16942b60eed4582169aeb297968ca2c
Kat Nix <deutschkat29@gmail.com>
2022-12-11 22:31:20 -0600

    exercise 1 A finished

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise1Answered/MenuController.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise1Answered/MenuController.java
@@ -10,7 +10,9 @@ public class MenuController {
 
         //Guest One ordered Shrimp Alfredo - So we print out the details and run the method plateDish that has overridden the original plateDish.
 
-        System.out.println("Guest One ordered: " + guestOne.getNumServings() + " serving(s) of " + guestOne.getName() + ". This is an " + guestOne.getCulturalOrigin() + " " + guestOne.getFoodCategory()
+        System.out.println("=================================================");
+        System.out.println("Guest One ordered: " + guestOne.getNumServings() + " serving(s) of " + guestOne.getName() + ".");
+        System.out.println("This is an " + guestOne.getCulturalOrigin() + " " + guestOne.getFoodCategory()
                 + " dish that is " + guestOne.getCalories() + " calories and has " + guestOne.getNoodleType() + " noodles and " + guestOne.getSauceType() + " sauce.");
         System.out.println("To plate " + guestOne.getName() + ": ");
 
@@ -19,7 +21,9 @@ public class MenuController {
         //Guest Two ordered Spaghetti - So we print out the details again, and run the method plateDish that is found in the Pasta class.
         // Because spaghetti is pretty basic pasta, so it can be plated like a basic pasta.
 
-        System.out.println("Guest Two ordered: " + guestTwo.getNumServings() + " serving(s) of " + guestTwo.getName() + ". This is an " + guestTwo.getCulturalOrigin() + " " + guestTwo.getFoodCategory()
+        System.out.println("=================================================");
+        System.out.println("Guest Two ordered: " + guestTwo.getNumServings() + " serving(s) of " + guestTwo.getName() + ".");
+        System.out.println("This is an " + guestTwo.getCulturalOrigin() + " " + guestTwo.getFoodCategory()
                 + " dish that is " + guestTwo.getCalories() + " calories and has " + guestTwo.getNoodleType() + " noodles and " + guestTwo.getSauceType() + " sauce.");
         System.out.println("To plate " + guestTwo.getName() + ": ");
 
@@ -28,7 +32,9 @@ public class MenuController {
         //Guest Three ordered Chicken Marsala - So we print out the details again, and run the method plateDish found in the Chicken Marsala class.
         //If we had another menu item, we could demonstrate the original plateDish method, however I did show 3 overrides.
 
-        System.out.println("Guest Three ordered: " + guestThree.getNumServings() + " serving(s) of " + guestThree.getName() + ". This is an " + guestThree.getCulturalOrigin() + " " + guestThree.getFoodCategory()
+        System.out.println("=================================================");
+        System.out.println("Guest Three ordered: " + guestThree.getNumServings() + " serving(s) of " + guestThree.getName() + ".");
+        System.out.println("This is an " + guestThree.getCulturalOrigin() + " " + guestThree.getFoodCategory()
                 + " dish that is " + guestThree.getCalories() + " calories.");
         System.out.println("To plate " + guestThree.getName() + ": ");
 
