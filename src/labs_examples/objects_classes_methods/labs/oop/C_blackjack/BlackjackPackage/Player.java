c0a0d490cdeb17241fcf499653e035717fe69f08
Kat Nix <deutschkat29@gmail.com>
2022-12-16 16:35:40 -0600

    Starting Blackjack

--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/BlackjackPackage/Player.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/BlackjackPackage/Player.java
@@ -1,4 +1,56 @@
 package labs_examples.objects_classes_methods.labs.oop.C_blackjack.BlackjackPackage;
 
 public class Player {
+
+    // Player Info
+
+    String name;
+    Hand hand;
+    int potValue;
+
+    //Constructor
+
+    public Player(String name, Hand hand, int potValue) {
+        this.name = name;
+        this.hand = hand;
+        this.potValue = potValue;
+    }
+
+    // Getter and Setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
+
+    public int getPotValue() {
+        return potValue;
+    }
+
+    public void setPotValue(int potValue) {
+        this.potValue = potValue;
+    }
+
+    // To string (if needed)
+
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", hand=" + hand +
+                ", potValue=" + potValue +
+                '}';
+    }
 }
