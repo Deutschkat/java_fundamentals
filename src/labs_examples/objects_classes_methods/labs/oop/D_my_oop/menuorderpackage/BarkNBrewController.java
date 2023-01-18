461b3e267ab4f100f9979a8259909246dd5b7ef2
Kat Nix <deutschkat29@gmail.com>
2023-01-17 21:53:00 -0600

    Bark N' Brew

---
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/menuorderpackage/BarkNBrewController.java
@@ -0,0 +1,19 @@
+package labs_examples.objects_classes_methods.labs.oop.D_my_oop.menuorderpackage;
+
+public class BarkNBrewController {
+    public static void main(String[] args) {
+
+        BarkNBrewController barkNBrewController = new BarkNBrewController();
+        barkNBrewController.welcomeInfo();
+
+    }
+
+    public void welcomeInfo(){
+        System.out.println("Welcome To Bark N' Brew!");
+        System.out.println();
+        System.out.println("-------------------------------------------------------");
+
+
+    }
+
+}
