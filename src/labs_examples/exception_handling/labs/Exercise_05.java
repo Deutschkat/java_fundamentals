b355fc92ac72efdf16299e3cbfbc62fe8c14b79d
Kat Nix <deutschkat29@gmail.com>
2023-02-04 19:58:06 -0600

    Ex5 Exceptions

--- src/labs_examples/exception_handling/labs/Exercise_05.java
+++ src/labs_examples/exception_handling/labs/Exercise_05.java
@@ -13,6 +13,25 @@ class Example {
 
     public static void main(String[] args) {
 
+        try{
+            methodOne();
+        }catch(ArrayIndexOutOfBoundsException exc){
+            System.out.println("Exception!");
+        }
+
+    }
+
+    public static void methodOne() throws ArrayIndexOutOfBoundsException{
+
+        int[] nums = {1,2,3,4,5};
+
+        try {
+            int x = nums[20];
+        }catch(ArrayIndexOutOfBoundsException exc){
+            System.out.println("Exception caught in method.");
+            System.out.println("Throwing back now...");
+            throw exc;
+        }
     }
 
 }
