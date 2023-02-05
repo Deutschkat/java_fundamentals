fcdf9e4359620cc5d1dd0f38d53aaaefd6fdbd3e
Kat Nix <deutschkat29@gmail.com>
2023-02-04 19:46:08 -0600

    Ex2 Exceptions

---
+++ src/labs_examples/exception_handling/labs/Ex2.java
@@ -0,0 +1,18 @@
+package labs_examples.exception_handling.labs;
+
+public class Ex2 {
+    public static void main(String[] args) {
+
+
+        int[] nums = {6, 7, 8, 9, 0};
+
+        try {
+            int x = nums[0] / nums[4];
+        } catch (ArithmeticException aex) {
+            System.out.println("This number not divisible by zero.");
+        } catch (ArrayIndexOutOfBoundsException ex){
+            System.out.println("Error.");
+        }
+
+    }
+    }
