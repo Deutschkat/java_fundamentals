44d5e7731abaad60c1e99f2a5f7da3295d4f4317
Kat Nix <deutschkat29@gmail.com>
2023-02-14 19:18:53 -0600

    ex 1 input output

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -13,39 +13,45 @@ import java.io.*;
 
 class Example {
     public static void main(String[] args) {
+            BufferedInputStream inputStream = null;
+            BufferedOutputStream outputStream = null;
 
-        BufferedInputStream inputStream = null;
-        BufferedOutputStream outputStream = null;
-
-        try{
-            inputStream = new BufferedInputStream(new FileInputStream("C:\\Users\\deuts\\Documents\\CodingNomads\\labs\\online-java-fundamentals\\src\\labs_examples\\repl\\example.txt"));
-            outputStream = new BufferedOutputStream(new FileOutputStream("C:\\Users\\deuts\\Documents\\CodingNomads\\labs\\online-java-fundamentals\\src\\labs_examples\\input_output\\example.txt"));
-
-            byte[] byteArray;
-
-            while((byteArray = inputStream.readNBytes(5)) != null){
-                outputStream.write(byteArray,0,5);
-
-            }
+            String inputFilePath = "C:\\Users\\deuts\\Documents\\CodingNomads\\labs\\online-java-fundamentals\\src\\labs_examples\\input_output\\labs\\inputtext.txt";
+            String outputFilePath = "C:\\Users\\deuts\\Documents\\CodingNomads\\labs\\online-java-fundamentals\\src\\labs_examples\\input_output\\labs\\outputtext.txt";
 
 
-        }catch(IOException ex1){
-            System.out.println("An error occurred");
-        }finally{
             try{
-                inputStream.close();
-            }catch(IOException ex2){
-                ex2.printStackTrace();
+                inputStream = new BufferedInputStream(new FileInputStream(inputFilePath));
+                outputStream = new BufferedOutputStream(new FileOutputStream(outputFilePath));
+
+                byte[] byteArray = new byte[8];
+                int bytesRead = 0;
+
+                while((bytesRead = inputStream.read(byteArray)) != -1){
+
+                    outputStream.write(new String(byteArray, 0, bytesRead).getBytes());
+
+                }
+
+                System.out.println("File written!");
+
+            }catch(IOException ex1){
+                System.out.println("An error occurred");
+            }finally{
+                try{
+                    inputStream.close();
+                }catch(IOException ex2){
+                    ex2.printStackTrace();
+                }
+                try{
+                    outputStream.close();
+                }catch(IOException ex2){
+                    ex2.printStackTrace();
+                }
             }
-            try{
-                outputStream.close();
-            }catch(IOException ex2){
-                ex2.printStackTrace();
-            }
+
+
+
+
         }
-
-
-
-
-    }
 }
