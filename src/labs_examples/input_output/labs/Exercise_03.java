af6afd6faed1a8f4c950e2ac3287629568fcc4f9
Kat Nix <deutschkat29@gmail.com>
2023-02-24 22:51:26 -0600

    Labs

--- src/labs_examples/input_output/labs/Exercise_03.java
+++ src/labs_examples/input_output/labs/Exercise_03.java
@@ -5,12 +5,15 @@ package labs_examples.input_output.labs;
  *
  *    1) Demonstrate the usage of at least two additional Byte Streams
  *    // FileInputStream & ByteArrayInputStream & StringBufferInputStream
+ *    Check, Check
  *
  *    2) Demonstrate the usage of at least two additional Character Streams
  *    //FileReader & CharArrayReader
+ *     Check,
  *
  *    3) Demonstrate using a buffer on one of the Byte Streams and one of the Character Streams
  *    // Byte stream BufferedInputStream and a Character stream BufferedWriter
+ *      Check,
  *
  *    4) Demonstrate the use of the DataInputStream and DataOutputStream
  *
