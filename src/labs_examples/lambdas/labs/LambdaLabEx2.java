d4b23d40b641cc49036ce96fb4dc26dd34b02d1a
Kat Nix <deutschkat29@gmail.com>
2023-02-26 16:04:37 -0600

    Labs

--- src/labs_examples/lambdas/labs/LambdaLabEx2.java
+++ src/labs_examples/lambdas/labs/LambdaLabEx2.java
@@ -1,10 +1,13 @@
 package labs_examples.lambdas.labs;
 
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.function.Function;
-import java.util.function.Supplier;
+import java.util.function.*;
+
+import static java.lang.Math.PI;
+
 
 public class LambdaLabEx2 {
     public static void main(String[] args) {
@@ -29,8 +32,48 @@ public class LambdaLabEx2 {
 
         System.out.println(divide.apply(77));
 
-        // 4)
+        // 4) Predicate - Returns a boolean.
 
+        Predicate<Integer> lessThan = j -> (j < 7);
+
+        System.out.println(lessThan.test(2));
+        System.out.println(lessThan.test(15));
+
+        // 5) Binary Operator
+        BinaryOperator <Integer> testBinary = (a,  b) -> {
+            int sum = a + b;
+            return sum;
+        };
+        System.out.println(testBinary.apply(5,5));
+
+        // 6) IntToDouble - Dealing with Ints in argument, result becomes a double.
+
+        IntToDoubleFunction piDividedByS = (s) -> PI / s ;
+        System.out.println(piDividedByS.applyAsDouble(5));
+
+
+        // 7) ObjIntConsumer
+
+        ObjIntConsumer<String> test2 = (d, c) -> System.out.println(d + c);
+        test2.accept("Ninja Turtles ", 4);
+
+        // 8) UnaryOperator
+        System.out.println("Yell something in all upper case to have it quieted down to lower case!");
+
+        UnaryOperator<String> words = (x) -> x.toLowerCase();
+        System.out.println(words.apply(drScannington.nextLine()));
+
+        // 9) Bi Function
+        BiFunction<String, String, String> testBiFunction = (x, y) -> {
+            return x + y;
+        };
+
+        System.out.println(testBiFunction.apply("Kate ", "Nix"));
+
+        // 10) IntUnaryOperator
+        IntUnaryOperator meep = (c) -> c * c;
+
+        System.out.println(meep.applyAsInt(15));
 
     }
 }
