b79084f8ea5a82cdfc045b8c2963143b188403d5
Kat Nix <deutschkat29@gmail.com>
2023-02-27 17:58:27 -0600

    Labs

--- src/labs_examples/lambdas/labs/LambdaLabEx3.java
+++ src/labs_examples/lambdas/labs/LambdaLabEx3.java
@@ -1,6 +1,10 @@
 package labs_examples.lambdas.labs;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class LambdaLabEx3 {
     public static void main(String[] args) {
@@ -11,7 +15,24 @@ public class LambdaLabEx3 {
         System.out.println("Static method reference product is: " + x);
 
         // Instance method reference
+        // ** Did I do this correctly? =/ **
 
+        Singer singer1 = new Singer("David Bowie");
+        Song song1 = new Song("Ziggy Stardust");
+
+        singer1.execute(song1, singer1::sing);
+
+        Singer singer2 = new Singer("James Taylor");
+        Song song2 = new Song("Fire and Rain");
+
+        singer2.execute(song2, singer2::sing);
+
+        // Constructor method references
+        UserGenerator userGenerator = User::new;
+        User kat = userGenerator.createUser("Kat", 30);
+        User chris = userGenerator.createUser("Chris", 36);
+        System.out.println(kat.toString());
+        System.out.println(chris.toString());
 
 
 
@@ -20,4 +41,70 @@ public class LambdaLabEx3 {
     public static int add(int a, int b){
         return a + b;
     }
+}
+
+interface UserGenerator{
+    User createUser(String name, int age);
+}
+
+class User{
+    String name;
+    int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
+class Song {
+
+    private String name;
+
+    public Song(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+class Singer{
+    private String singerName;
+
+    public Singer(String singerName) {
+        this.singerName = singerName;
+    }
+
+    public String getSingerName() {
+        return singerName;
+    }
+
+    public void setSingerName(String singerName) {
+        this.singerName = singerName;
+    }
+
+    public void sing(Song x){
+        System.out.println( singerName+ " is singing the song: " + x.getName());
+
+
+    }
+
+    public void execute( Song song, Consumer<Song> x){
+        x.accept(song);
+    }
+
 }
---
+++ src/labs_examples/repl/MethodRefREPL.java
@@ -0,0 +1,30 @@
+package labs_examples.repl;
+
+import com.sun.tools.javac.Main;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MethodRefREPL {
+
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>();
+        nums.add(4567);
+        nums.add(345);
+        nums.add(2467);
+        nums.add(12);
+
+ //   Main object = new Main();
+
+    // Step 1) Modify the line below to use a method reference rather than a lambda expression
+   // Collections.sort(nums, (num1, num2) -> object.compare(num1, num2));
+
+    // Step 2) Modify the line below to use a method reference rather than a lambda expression
+    nums.forEach((n) -> System.out.println(n));
+}
+
+    public int compare(Integer val1, Integer val2) {
+        return val1.compareTo(val2);
+    }
+}
